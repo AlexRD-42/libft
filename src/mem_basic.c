13f7c7dc751a0d0e87d1ba44bc471c3341a038aa
Alex <alexdeimling@gmail.com>
2025-05-03 08:37:22 +0100

    Improved memcpy

--- src/mem_basic.c
+++ src/mem_basic.c
@@ -6,59 +6,95 @@
 /*   By: adeimlin <adeimlin@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 20:41:24 by adeimlin          #+#    #+#             */
-/*   Updated: 2025/04/22 13:32:08 by adeimlin         ###   ########.fr       */
+/*   Updated: 2025/05/03 08:33:46 by adeimlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 
-// Memset is int because of word copying
-void	*ft_memset(void *dst_void, uint8_t byte, size_t n)
+void	*ft_bzero(void *dst_void, size_t length)
 {
-	unsigned char	*dst;
+	char	*dst;
 
-	dst = (unsigned char *) dst_void;
-	while (n > 0)
+	dst = (char *) dst_void;
+	while (((uintptr_t)dst & (sizeof(void*) - 1)) && length > 0)
 	{
-		*dst = byte;
-		dst++;
-		n--;
+		*dst++ = 0;
+		length--;
+	}
+	while (length >= sizeof(void*))
+	{
+		*((uintptr_t *)dst) = 0;
+		dst += sizeof(void*);
+		length -= sizeof(void*);
+	}
+	while (length > 0)
+	{
+		*dst++ = 0;
+		length--;
 	}
 	return (dst_void);
 }
 
-int32_t	ft_memcmp(const void *addr1_void, const void *addr2_void, size_t data_size)
+// Maybe remove the length check
+void	*ft_memset(void *dst_void, const uint8_t byte, size_t length)
+{
+	char	*dst;
+	size_t	word_byte;
+
+	dst = (char *) dst_void;
+	if (length >= 16)
+	{
+		word_byte = (0x0101010101010101 & UINTPTR_MAX) * byte;
+		while (((uintptr_t)dst & (sizeof(void*) - 1)) && length-- > 0)
+			*dst++ = byte;
+		while (length >= sizeof(void*))
+		{
+			*((uintptr_t *)dst) = word_byte;
+			dst += sizeof(void*);
+			length -= sizeof(void*);
+		}
+	}
+	while (length > 0)
+	{
+		*dst++ = byte;
+		length--;
+	}
+	return (dst_void);
+}
+
+int32_t	ft_memcmp(const void *addr1_void, const void *addr2_void, size_t length)
 {
 	const uint8_t	*addr1 = (const uint8_t *) addr1_void;
 	const uint8_t	*addr2 = (const uint8_t *) addr2_void;
 
 	if (addr1_void == addr2_void)
 		return (0);
-	while (data_size > 0)
+	while (length > 0)
 	{
 		if (*addr1 != *addr2)
 			return (*addr1 - *addr2);
 		addr1++;
 		addr2++;
-		data_size--;
+		length--;
 	}
 	return (0);
 }
 
-int32_t	ft_memrcmp(const void *addr1_void, const void *addr2_void, size_t data_size)
+int32_t	ft_memrcmp(const void *addr1_void, const void *addr2_void, size_t length)
 {
-	const uint8_t	*addr1 = (const uint8_t *) addr1_void + data_size - 1;
-	const uint8_t	*addr2 = (const uint8_t *) addr2_void + data_size - 1;
+	const uint8_t	*addr1 = (const uint8_t *) addr1_void + length - 1;
+	const uint8_t	*addr2 = (const uint8_t *) addr2_void + length - 1;
 
 	if (addr1_void == addr2_void)
 		return (0);
-	while (data_size > 0)
+	while (length > 0)
 	{
 		if (*addr1 != *addr2)
 			return (*addr1 - *addr2);
 		addr1--;
 		addr2--;
-		data_size--;
+		length--;
 	}
 	return (0);
 }
--- src/new/ft_memcpy.c
+++ src/new/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: adeimlin <adeimlin@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 14:35:33 by adeimlin          #+#    #+#             */
-/*   Updated: 2025/04/23 22:47:27 by adeimlin         ###   ########.fr       */
+/*   Updated: 2025/05/03 08:36:27 by adeimlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,16 +49,82 @@ static inline void	ft_bwdcpy(uint8_t *dst, const uint8_t *src, size_t n)
 // 4. Perform tail copy byte by byte
 
 // Possible to index tail with word copying by and masking the excess and OR the result
-void	*ft_memcpy(void *dst_void, const void *src_void, size_t n)
-{
-	const size_t		words = n / sizeof(uintptr_t);
-	const unsigned char	*src = (const unsigned char *) src_void;
-	unsigned char		*dst;
 
-	dst = (unsigned char *) dst_void;
-	if (n >= 16)
+static void	*ft_memrcpy(void *dst_void, const char *src, size_t length)
+{
+	char	*dst;
+
+	dst = (char *) dst_void + length;
+	if (length >= 16)
 	{
-		
+		while (((uintptr_t)dst & (sizeof(uintptr_t) - 1)) && length-- > 0)
+			*--dst = *--src;
+		while (length >= sizeof(uintptr_t))
+		{
+			*((uintptr_t *)dst) = *((uintptr_t *)src);
+			dst -= sizeof(uintptr_t);
+			src -= sizeof(uintptr_t);
+			length -= sizeof(uintptr_t);
+		}
+	}
+	while (length > 0)
+	{
+		*--dst = *--src;
+		length--;
+	}
+	return (dst_void);
+}
+
+void	*ft_memcpy(void *dst_void, const void *src_void, size_t length)
+{
+	char		*dst;
+	const char	*src = (const char *) src_void;
+
+	dst = (char *) dst_void;
+	while (length > 2 * sizeof(int*) && ((uintptr_t)dst & (sizeof(int*) - 1)))
+	{
+		*dst++ = *src++;
+		length--;
+	}
+	while (length >= sizeof(int*))
+	{
+		*((uintptr_t *)dst) = *((uintptr_t *)src);
+		dst += sizeof(int*);
+		src += sizeof(int*);
+		length -= sizeof(int*);
+	}
+	while (length > 0)
+	{
+		*dst++ = *src++;
+		length--;
+	}
+	return (dst_void);
+}
+
+void	*ft_memmove(void *dst_void, const void *src_void, size_t length)
+{
+	char		*dst;
+	const char	*src = (const char *) src_void;
+
+	if ((uintptr_t) dst_void > (uintptr_t) src_void)
+		return (ft_memrcpy(dst_void, src + length, length));
+	dst = (char *) dst_void;
+	while (length > 2 * sizeof(int*) && ((uintptr_t)dst & (sizeof(int*) - 1)))
+	{
+		*dst++ = *src++;
+		length--;
+	}
+	while (length >= sizeof(int*))
+	{
+		*((uintptr_t *)dst) = *((uintptr_t *)src);
+		dst += sizeof(int*);
+		src += sizeof(int*);
+		length -= sizeof(int*);
+	}
+	while (length > 0)
+	{
+		*dst++ = *src++;
+		length--;
 	}
 	return (dst_void);
 }
